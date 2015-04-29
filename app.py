f114ebd0ff5f4a99f4b9dd7730e00e2956fdb66f
Rob Jefferson <rjefferson@solutionsforprogress.com>
2015-04-29 16:37:03 -0400

    Moving code from application to external module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 import numpy as np
+import asset_sim
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -37,25 +38,7 @@ def random_nums(draws):
 @app.route('/asset_sim/api/v1.0/wiener', defaults={'sample': 100})
 @app.route('/asset_sim/api/v1.0/wiener/<int:sample>', methods=["GET"])
 def wiener(sample):
-    """
-    Generates a wiener process of size sample, which is defined by the
-    following characteristics:
-
-    1)  W0 = 0;
-    2)  Wt is almost surely continuous;
-    3)  Wt has independent incremenets;  Wt-Ws ~ N(0, t-s), or a normal
-        distribution with mean 0 and variance (t-s)^2.
-    """
-    W = np.zeros(sample, np.dtype(float))
-
-    mu, sig = 0, 1
-
-    dt = (1/sample)**(1/2)
-
-    for i in range(1, sample):
-        W[i] = W[i-1] + np.random.normal(mu, sig)*dt
-
-    return(jsonify(dict(enumerate(W))))
+    return jsonify(asset_sim.wiener(sample))
 
 if __name__ == '__main__':
     manager.run()
--- asset_sim.py
+++ asset_sim.py
@@ -1,23 +1,34 @@
 import numpy as np
 
-class GeomBrownianMotion:
-    def __init__(self, s0, time, time_rate, sigma, mu):
-        # s0 is the initial asset price
-        self.s0 = s0
-        # time is the amount of time covered
-        self.time = time
-        # time_rate is the number of periods in each unit of time
-        # the product of time_rate and time is the number of steps
-        # in the simulation
-        self.time_rate = time_rate
-        self.sigma = sigma
-        self.mu = mu
+
+def wiener(sample):
+    """
+    Generates a wiener process of size sample, which is defined by the
+    following characteristics:
+
+    1)  W0 = 0;
+    2)  Wt is almost surely continuous;
+    3)  Wt has independent incremenets;  Wt-Ws ~ N(0, t-s), or a normal
+        distribution with mean 0 and variance (t-s)^2.
+    """
+    W = np.zeros(sample, np.dtype(float))
+
+    mu, sig = 0, 1
+
+    dt = (1/sample)**(1/2)
+
+    for i in range(1, sample):
+        W[i] = W[i-1] + np.random.normal(mu, sig)*dt
+
+    return dict(enumerate(W))
+
 
 def generate_returns(log_returns):
     """
     Given a list of log returns, give back a list of returns.
     """
     return np.exp(log_returns)
+
 
 def generate_prices(params, log_returns):
     """
@@ -31,4 +42,3 @@ def generate_prices(params, log_returns):
     for i in range(1, len(returns)):
         price_seq.append(price_seq[i-1]*returns[i-1])
     return np.array(price_seq)
-
