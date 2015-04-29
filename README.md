7156396e21011041934e9f0d7b227bfcb0aed445
Rob Jefferson <rjefferson@solutionsforprogress.com>
2015-04-29 15:09:40 -0400

    Added support for Wiener processes

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
-# asset_sim
+# Asset Simulation Project 
 Attempt at developing an asset price simulator/API in Python
 
 I want to learn to create APIs for data, that I can use to display said data on a website.
 
 Nothing here is to simulate real stock or asset pricing activity, except perhaps in the broadest statistical sense.
 
-Currently, the API just generates IID draws from a standard normal distribution. Over time,
-I want to simulate Wiener processes, geometric Brownian motion, along with implementing a streaming
-API.
+Currently, the API does the following:
+* Generates IID draws from a standard normal distribution
+* Generates samples of a Wiener process (i.e. standard Brownian motion)
+
+What it doesn't do:
+* Generates sample paths for geometric Brownian motion
+* Stream the data to the client
+
+# Installation
+
+First, create a virtualenv:
+
+```bash
+virtualenv --python=python3.4 ~/asset_sim_env
+```
+
+Then, start the virtualenv:
+
+```bash
+source ~/asset_sim_env/bin/activate
+```
+
+Then, install the Python modules:
+
+```bash
+cd asset_sim/reqs
+pip install -r requirements.txt
+```
+
--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def ua():
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.route('/asset_sim/api/v1.0/random/', defaults={'draws': 10})
 @app.route('/asset_sim/api/v1.0/random/<int:draws>', methods=["GET"])
 def random_nums(draws):
@@ -32,6 +33,29 @@ def random_nums(draws):
     """
     return jsonify(dict(enumerate(np.random.standard_normal(draws))))
 
+
+@app.route('/asset_sim/api/v1.0/wiener', defaults={'sample': 100})
+@app.route('/asset_sim/api/v1.0/wiener/<int:sample>', methods=["GET"])
+def wiener(sample):
+    """
+    Generates a wiener process of size sample, which is defined by the
+    following characteristics:
+
+    1)  W0 = 0;
+    2)  Wt is almost surely continuous;
+    3)  Wt has independent incremenets;  Wt-Ws ~ N(0, t-s), or a normal
+        distribution with mean 0 and variance (t-s)^2.
+    """
+    W = np.zeros(sample, np.dtype(float))
+
+    mu, sig = 0, 1
+
+    dt = (1/sample)**(1/2)
+
+    for i in range(1, sample):
+        W[i] = W[i-1] + np.random.normal(mu, sig)*dt
+
+    return(jsonify(dict(enumerate(W))))
 
 if __name__ == '__main__':
     manager.run()
